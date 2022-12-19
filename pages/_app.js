9ab649b48ef6ccb78fa24f4832996b5294435666
turtle <769711153@qq.com>
2022-12-19 20:49:49 +0800

    构建失败了. 彻底移除store, 使得构建成功

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,12 @@
 import 'normalize.css/normalize.css';
 import '../styles/global.css'
 import NextNProgress from 'nextjs-progressbar';
-import store from '../store/store'
 import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps }) {
-  return <Provider store={store}>
+  return <>
   <NextNProgress></NextNProgress>  
   <Component {...pageProps} />
 
-  </Provider>;
+  </>;
 }
